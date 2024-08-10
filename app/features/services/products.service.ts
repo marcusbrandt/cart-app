8ae10546856a98007695be863208cfd0f8aac027
Marcus Brandt <marcusbrandt@gmail.com>
2024-08-10 14:24:46 -0400

    refactor: Change service layer to new file

---
+++ app/features/services/products.service.ts
@@ -0,0 +1,6 @@
+const productsUrl =
+  'https://gist.githubusercontent.com/marcusbrandt/8507b74f6cc0247c9656a228cf0b3475/raw/92a63a70eec517932c9c6ebc26bec1dcdaf8621f/products.json';
+
+export const fetchProducts = async () => {
+  return fetch(productsUrl);
+};
--- config/config.ts
+++
@@ -1,1 +0,0 @@
-export const Products = 'https://gist.githubusercontent.com/marcusbrandt/8507b74f6cc0247c9656a228cf0b3475/raw/92a63a70eec517932c9c6ebc26bec1dcdaf8621f/products.json';
