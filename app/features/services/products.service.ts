68e69e377b9151e6142d5454ce24911cd6d98f2c
Marcus Brandt <marcusbrandt@gmail.com>
2024-08-10 20:04:18 -0400

    refactor: Add new url to fetch
    Now with image url

--- app/features/services/products.service.ts
+++ app/features/services/products.service.ts
@@ -4,7 +4,7 @@ export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({
     baseUrl:
-      'https://gist.githubusercontent.com/marcusbrandt/8507b74f6cc0247c9656a228cf0b3475/raw/92a63a70eec517932c9c6ebc26bec1dcdaf8621f',
+      'https://gist.githubusercontent.com/marcusbrandt/8507b74f6cc0247c9656a228cf0b3475/raw/ab549abf10dda781e4bdbe4bfbdf23edf6b10bae/',
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
